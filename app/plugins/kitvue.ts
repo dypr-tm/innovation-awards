372d4abbef8f2afd3308e7e9451741e574ec635c
Cindyo Dhelistya S P <poj@NB-P93280.lan>
2026-04-16 01:02:53 +0700

    fix: temporary bypass kitvue for successful deploy

--- app/plugins/kitvue.ts
+++ app/plugins/kitvue.ts
@@ -1,6 +1,6 @@
-import PegadaianUIKit from '@pegadaian/kitvue'
-import '@pegadaian/kitvue/dist/style.css'
+// import PegadaianUIKit from '@pegadaian/kitvue'
+// import '@pegadaian/kitvue/dist/style.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(PegadaianUIKit)
+  // nuxtApp.vueApp.use(PegadaianUIKit)
 })
